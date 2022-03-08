0597a01feacdeea8c4da3f02c304e222f112208d
Manu <manu.villar.vitoria@gmail.com>
2022-03-08 17:45:03 +0100

    Rest Api Controller and unit testing approach with mockito

--- src/main/java/com/weCode/bookStore/dto/BookDto.java
+++ src/main/java/com/weCode/bookStore/dto/BookDto.java
@@ -1,12 +1,10 @@
 package com.weCode.bookStore.dto;
 
-import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
 import java.util.UUID;
 
 @Data
