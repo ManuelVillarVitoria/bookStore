7181c695965da09da70d8b566c65d1aba2e6b270
Manu <manu.villar.vitoria@gmail.com>
2022-03-08 16:17:48 +0100

    Service layer and Unit testing approach with Mockito

--- src/main/java/com/weCode/bookStore/dto/BookDto.java
+++ src/main/java/com/weCode/bookStore/dto/BookDto.java
@@ -1,14 +1,24 @@
 package com.weCode.bookStore.dto;
 
+import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import java.util.UUID;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class BookDto {
+    private UUID id;
+
     private String title;
+
+    private String description;
+
+    private int releaseYear;
 }
